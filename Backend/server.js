d3c0b3edda80a8ed6909981d19465f324ac90b03
Gunadi <edward.stiawan28@gmail.com>
2026-05-19 12:43:17 +0700

    Update server.js untuk Vercel di branch gunadi

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use('/api/chats', chatRoutes);
 app.use('/api/language',languageRoutes)
 
 // Mengambil port dan URL database dari file .env
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 const dbURI = process.env.MONGO_URI;
 
 // Rute dasar untuk mengetes server
@@ -46,10 +46,12 @@ app.get('/', (req, res) => {
 mongoose.connect(dbURI)
   .then(() => {
     console.log('✅ Berhasil terhubung ke MongoDB Atlas!');
-    app.listen(PORT, () => {
-      console.log(`🚀 Server berjalan pada port ${PORT}`);
-    });
+//    app.listen(PORT, () => {
+//      console.log(`🚀 Server berjalan pada port ${PORT}`);
+//    });
   })
   .catch((err) => {
     console.error('❌ Gagal terhubung ke MongoDB:', err);
   });
+
+  module.exports = app;
